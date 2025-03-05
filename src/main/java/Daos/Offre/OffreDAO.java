7da2d582050ed751b43fd5fd48e1fb843aabe4da
stouf <ouadad.mustapha01@gmail.com>
2025-03-05 15:59:19 +0000

    getParameter dans servlet metode Post

--- src/main/java/Daos/Offre/OffreDAO.java
+++ src/main/java/Daos/Offre/OffreDAO.java
@@ -1,7 +1,10 @@
 package Daos.Offre;
 
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,34 +12,59 @@ import Models.Offre.Offre;
 
 public class OffreDAO {
 	
-	private Connection coon;
+	private static Connection coon;
 
 	public OffreDAO(Connection coon) {
 		super();
 		this.coon = coon;
 	}
 
-	public void AjouterOffer(Offre offre) {
+	public static int AjouterOffer(Offre offre) {
+		int result=0;
 		try {
 			String sql="INSERT INTO OffreEmploi(Titre,Description,category)values(?,?,?)";
 			PreparedStatement stm= coon.prepareStatement(sql);
 			stm.setString(1, offre.getTitre());
 			stm.setString(2, offre.getDescription());
 			stm.setString(3, offre.getCategory());
-			stm.executeUpdate();
+			result=stm.executeUpdate();
 			
 			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return result;
 		
 		
 	}
 	
 	
-	  public List<Offre> getAllOffres(){
-		  List<Offre> offres=new ArrayList<>();
+	  public static List<Offre> getAllOffres(){
+		  List<Offre> offres=new ArrayList<Offre>();
+		  
+		  try {
+			  String sql ="SELECT * FROM OffreEmploi";
+			  Statement stm = coon.createStatement();
+			  ResultSet rs = stm.executeQuery(sql);
+			  while (rs.next()) {
+				  Offre offre = new Offre(
+						  rs.getString("titre"),
+						  rs.getString("description"),
+						  rs.getString("category")
+						  );
+				  
+				  offre.setDatePublication(rs.getTimestamp("datePublication"));
+				  offres.add(offre);
+				  
+				
+			}
+			
+		} catch (Exception e) {
+			  e.printStackTrace();
+		}
+		  
+		  
 		  return offres;
 	  }
 
--- src/main/java/Servlets/OffreServlet.java
+++ src/main/java/Servlets/OffreServlet.java
@@ -1,6 +1,7 @@
 package Servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,32 +9,45 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import Daos.Offre.OffreDAO;
+import Models.Offre.Offre;
+
 
 @WebServlet("/OffreServlet")
 public class OffreServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-    /**
-     * Default constructor. 
-     */
+	
     public OffreServlet() {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String titre=request.getParameter("titre");
+		String description=request.getParameter("description");
+		String category=request.getParameter("category");
+		
+		Offre offre= new Offre();
+		offre.setTitre(titre);
+		offre.setDescription(description);
+		offre.setCategory(category);
+		
+		
+		int result =OffreDAO.AjouterOffer(offre);
+		 if (result>0) {
+			 response.sendRedirect("");
+			
+		} else {
+			PrintWriter out = response.getWriter();
+			out.println("Data not add ");
+
+		}
+		
 	}
 
 }
